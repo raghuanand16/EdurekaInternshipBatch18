e4f9d77f4b234fbbfb2b27f46e3e9a72de8f48e3
Raghu <raghu.anand16@gmail.com>
2021-07-10 20:06:33 +0530

    10jul2021

---
+++ Javascript/index.js
@@ -0,0 +1,15 @@
+      var a = 100;
+      var b = "India";
+      var c = undefined;
+      var d = [1,2,3,4,5];
+      var x=5,y=5;
+      var e = (x==y);
+      var f = {Name: "Raghu", Age: 29};
+      document.getElementById("xyz1").innerHTML = typeof(a);
+      document.getElementById("xyz2").innerHTML = typeof(b);
+      document.getElementById("xyz3").innerHTML = typeof(c);
+      document.getElementById("xyz4").innerHTML = typeof(d);
+      document.getElementById("xyz5").innerHTML = typeof(e);
+      document.getElementById("xyz6").innerHTML = typeof(f);
+      //index.html => h1 tag => content ====
+// This is comment in JS
