29903fd737db1e5a18f70ad40c34179cb32ee2e9
Raghu <raghu.anand16@gmail.com>
2021-07-11 20:06:34 +0530

    11july2021

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,15 +1,17 @@
-      var a = 100;
-      var b = "India";
-      var c = undefined;
-      var d = [1,2,3,4,5];
-      var x=5,y=5;
-      var e = (x==y);
-      var f = {Name: "Raghu", Age: 29};
-      document.getElementById("xyz1").innerHTML = typeof(a);
-      document.getElementById("xyz2").innerHTML = typeof(b);
-      document.getElementById("xyz3").innerHTML = typeof(c);
-      document.getElementById("xyz4").innerHTML = typeof(d);
-      document.getElementById("xyz5").innerHTML = typeof(e);
-      document.getElementById("xyz6").innerHTML = typeof(f);
-      //index.html => h1 tag => content ====
-// This is comment in JS
+var abc = ["apple","orange","banana"];    //array of string
+//var abclen = abc.length;      //abclen will have the length of the array
+var list = "<ul>";
+for(var a = 0; a < abc.length; a++)  
+// a++ =  a + 1   
+//0<3 = apple, 1<3 = orange, 2<3 = banana, 3<3 => stop
+{
+      list += "<li>" + abc[a] + "</li>"; 
+}
+list += "</ul>";
+document.getElementById("xyz1").innerHTML = list;
+list => 
+<ul>
+      <li>apple</li>
+      <li>orange</li>
+      <li>banana</li>
+</ul>
